2c06b9723987bc3ee7dc7061914ca4cc2bbb56f2
HariniSathish10 <harinikalaesh@gmail.com>
2026-05-12 17:09:13 +0530

    Update OnlineShoppingAppApplication.java

--- onlineshoppingapp/onlineshoppingapp/src/main/java/com/example/onlineshoppingapp/OnlineShoppingAppApplication.java
+++ onlineshoppingapp/onlineshoppingapp/src/main/java/com/example/onlineshoppingapp/OnlineShoppingAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShoppingAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingappApplication.class, args);
+		SpringApplication.run(OnlineShoppingAppApplication.class, args);
 	}
 
 }
